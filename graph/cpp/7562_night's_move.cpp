9465655f5232aabe5ed1fc9085ff65cbf809d7f7
pletain <pletain@dgu.ac.kr>
2022-05-06 13:53:51 +0000

    update 5/6

---
+++ graph/cpp/7562_night's_move.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<queue>
+#include<utility>
+using namespace std;
+
+typedef pair<int> ii;
+typedef vector<int> vi;
+
+int main() {
+    int T, L;
+    cin >> T;
+    for(int i = 0; i < T; i++) {
+        int x, y;
+        ii cur, fin;
+        cin >> L;
+        int board = [L][L];
+        
+        cin >> x >> y;
+        cur = make_pair(x, y);
+        cin >> x >> y;
+        fin = make_pair(x, y);
+        
+        
+    }
+}
